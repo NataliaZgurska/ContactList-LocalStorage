8e962c3443c969eb2983b503b33a4962ed1cf8e6
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-03-24 23:26:35 +0200

    update styles

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,6 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from "yup";
-
+import css from './ContactForm.module.css'
 
 const FORM_INITIAL_VALUES = {
     name: "",
@@ -17,34 +17,35 @@ const ContactForm = ({ onAdd }) => {
   const handleSubmit = (values, actions) => {
       console.log(values);
        onAdd(values);
-		actions.resetForm();
+    actions.resetForm();
 	};
 
 
   return (
+    <div className={css.formAddContainer}>
       <Formik
           initialValues={ FORM_INITIAL_VALUES }
           validationSchema={FormSchema}
           onSubmit={handleSubmit}>
-          <Form>
-              <label>
+          <Form className={css.formAdd}>
+              <label className={css.formLabel}>
                   <span>Name:</span>
-                  <br />
+                
                   <Field type="text" name="name" />
                   <ErrorMessage component="p" name='name'/>
               </label>
-<br />
-             <label>
+
+             <label className={css.formLabel}>
                   <span>Number:</span>
-                  <br />
+                 
                   <Field type="number" name="number" />
                      <ErrorMessage component="p" name='number'/>
             </label>
-              <br />   
-<br />              
-			<button type="submit">Submit</button>
+   
+			      <button type="submit" className={css.formAddBtn}>Submit</button>
 			</Form>
-    </Formik>
+      </Formik>
+      </div>
   );
 }
 
