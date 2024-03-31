97d7083e7f42029ef65810a3336f4d9001e3fd35
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-03-31 18:15:24 +0300

    update add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,46 @@
-import { useEffect, useState } from 'react'
-import { nanoid } from 'nanoid'
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
 
-import initialContacts from './myContacts.json'
-import ContactList from './components/ContactList/ContactList'
-import SearchBox from './components/SearchBox/SearchBox'
-import ContactForm from './components/ContactForm/ContactForm'
-
-
+import initialContacts from './myContacts.json';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactForm from './components/ContactForm/ContactForm';
 
 function App() {
   const [contacts, setContacts] = useState(() => {
-    const stringifiedContacts = localStorage.getItem("contacts");
+    const stringifiedContacts = localStorage.getItem('contacts');
     if (!stringifiedContacts) return initialContacts;
     const parseContacts = JSON.parse(stringifiedContacts);
     return parseContacts;
   });
 
   useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-    
-  const [filter, setFilter] = useState("");
-  const visibleContacts = contacts.filter((item)=>item.name.toLowerCase().includes(filter.toLowerCase()))
+  const [filter, setFilter] = useState('');
+  const visibleContacts = contacts.filter(item =>
+    item.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-  const addContact = (newContact) => {
-    const finalContact = { ...newContact, id: nanoid(), };
-      setContacts((prevContacts) => [...prevContacts, finalContact])
+  const addContact = newContact => {
+    const finalContact = { ...newContact, id: nanoid() };
+    setContacts(prevContacts => [...prevContacts, finalContact]);
   };
 
-  const deleteContact = (contactId) => {
-    setContacts((prevContacts) => {
-     return prevContacts.filter((contact) => contact.id!==contactId)
-     })
-  }
+  const deleteContact = contactId => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(contact => contact.id !== contactId);
+    });
+  };
   return (
- <div>
+    <div>
       <h1>Phonebook</h1>
-      <ContactForm onAdd={addContact } />
-      <SearchBox value={filter} onFilter={setFilter } />
+      <ContactForm onAdd={addContact} />
+      <SearchBox value={filter} onFilter={setFilter} />
       <ContactList contacts={visibleContacts} onDelete={deleteContact} />
-</div>
-
-  )
+    </div>
+  );
 }
 
-export default App
-    
+export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,95 +1,123 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import * as Yup from "yup";
-import css from './ContactForm.module.css'
+import * as Yup from 'yup';
+import css from './ContactForm.module.css';
 
 const FORM_INITIAL_VALUES = {
-    name: "",
-    number: "",
-  favColor: "",
-    description: "",
-}
+  name: '',
+  number: '',
+  favColor: '',
+  description: '',
+};
 
 const FormSchema = Yup.object().shape({
-  name: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required"),
-  number: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required"),
+  name: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  number: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
   favColor: Yup.string()
-    .required("Favorite color is required!")
-    .oneOf(["red", "green", "blue"],
-      "Favorite color must be one of following: red, green, blue"),
-  description: Yup.string().min(8, "Too Short!").max(50, "Too Long!").required("Required"),
-    sex: Yup.string()
-    .required("required!"),
+    .required('Favorite color is required!')
+    .oneOf(
+      ['red', 'green', 'blue'],
+      'Favorite color must be one of following: red, green, blue'
+    ),
+  description: Yup.string()
+    .min(8, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  sex: Yup.string().required('required!'),
 });
-
 
 const ContactForm = ({ onAdd }) => {
   const handleSubmit = (values, actions) => {
     onAdd(values);
     actions.resetForm();
-	};
-
+  };
 
   return (
     <div className={css.formAddContainer}>
       <Formik
-          initialValues={ FORM_INITIAL_VALUES }
-          validationSchema={FormSchema}
-          onSubmit={handleSubmit}>
+        initialValues={FORM_INITIAL_VALUES}
+        validationSchema={FormSchema}
+        onSubmit={handleSubmit}
+      >
         <Form className={css.formAdd}>
+          <label className={css.formLabel}>
+            <span>Name:</span>
+            <Field type="text" name="name" />
+            <ErrorMessage
+              className={css.errorMessage}
+              component="p"
+              name="name"
+            />
+          </label>
 
           <label className={css.formLabel}>
-              <span>Name:</span>
-              <Field type="text" name="name" />
-              <ErrorMessage className={css.errorMessage} component="p" name='name'/>
+            <span>Number:</span>
+            <Field type="number" name="number" />
+            <ErrorMessage
+              className={css.errorMessage}
+              component="p"
+              name="number"
+            />
           </label>
-          
-            <label className={css.formLabel}>
-                  <span>Number:</span>
-                  <Field type="number" name="number" />
-                  <ErrorMessage className={css.errorMessage} component="p" name='number'/>
-          </label>
-                 
 
-            <div >
-              <span>Favorite color:</span>
-              <div className={css.radioBtn}>
-                  <label className={css.formRadio}>
-                      <span style={{color: 'green'}}>Green:</span>
-                      <Field type="radio" value="green" name="favColor" />
-                </label>
-                <label className={css.formRadio}>
-                      <span style={{color: 'blue'}}>Blue:</span>
-                      <Field type="radio" value="blue" name="favColor" />
-                  </label>   
-                  <label className={css.formRadio}>
-                      <span style={{color: 'red'}}>Red:</span>
-                      <Field type="radio" value="red" name="favColor" />
-                  </label>          
-                  <ErrorMessage className={css.errorMessage} component="p" name='favColor'/> 
-              </div>           
+          <div>
+            <span>Favorite color:</span>
+            <div className={css.radioBtn}>
+              <label className={css.formRadio}>
+                <span style={{ color: 'green' }}>Green:</span>
+                <Field type="radio" value="green" name="favColor" />
+              </label>
+              <label className={css.formRadio}>
+                <span style={{ color: 'blue' }}>Blue:</span>
+                <Field type="radio" value="blue" name="favColor" />
+              </label>
+              <label className={css.formRadio}>
+                <span style={{ color: 'red' }}>Red:</span>
+                <Field type="radio" value="red" name="favColor" />
+              </label>
+              <ErrorMessage
+                className={css.errorMessage}
+                component="p"
+                name="favColor"
+              />
             </div>
+          </div>
 
-            <label className={css.formLabel}>
-                  <span>Sex:</span>
-                 <Field as="select" name='sex' >
-                    <option value="women">Women</option>
-                    <option value="man">Men</option>
-                 </Field>
-                  <ErrorMessage className={css.errorMessage} component="p" name='sex'/>
-            </label>
-            
-        
-             <label className={css.formLabel}>
-                  <span>Description:</span>
-                  <Field as="textarea" name="description"  />
-                  <ErrorMessage className={css.errorMessage} component="p" name='description'/>
-            </label>
-            
-			   <button type="submit" className={css.formAddBtn}>Submit</button>
-			</Form>
+          <label className={css.formLabel}>
+            <span>Sex:</span>
+            <Field as="select" name="sex">
+              <option value="women">Women</option>
+              <option value="man">Men</option>
+            </Field>
+            <ErrorMessage
+              className={css.errorMessage}
+              component="p"
+              name="sex"
+            />
+          </label>
+
+          <label className={css.formLabel}>
+            <span>Description:</span>
+            <Field as="textarea" name="description" />
+            <ErrorMessage
+              className={css.errorMessage}
+              component="p"
+              name="description"
+            />
+          </label>
+
+          <button type="submit" className={css.formAddBtn}>
+            Submit
+          </button>
+        </Form>
       </Formik>
-      </div>
+    </div>
   );
-}
+};
 
-export default ContactForm
+export default ContactForm;
